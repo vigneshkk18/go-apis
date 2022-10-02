ce0cd4067739ed76b41727da333b761bd219d88a
Vignesh <kkvignesh2000@gmail.com>
2022-10-02 14:08:08 +0530

    removed load env vars from .env

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -3,17 +3,11 @@ package initializers
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 var DB_URI string
 
 func LoadEnvVariables() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("No .env file found")
-	}
-
 	DB_URI = os.Getenv("MONGODB_URI")
 
 	if DB_URI == "" {
