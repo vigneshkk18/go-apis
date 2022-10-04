5f2c90cdcee138cb4ca3f8e64904f9e36013b852
Vignesh <kkvignesh2000@gmail.com>
2022-10-04 13:19:25 +0530

    added cors middleware

--- routes/routes.go
+++ routes/routes.go
@@ -4,7 +4,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func InitializeRoutes(r *gin.Engine) {
+	r.Use(CORSMiddleware())
+
 	// Non - Project Specific Routes
 	r.GET("/generate-paragraph/:difficulty", GetRandomParagraph)
 
